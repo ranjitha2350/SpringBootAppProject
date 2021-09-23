db20e4fba7d274675f3724621cbadd75649ca26c
rvattipu <rvattipu@RVATTIPU-WT1.cisco.com>
2021-09-23 19:49:41 +0530

    Wh: testcase "first commit"

---
+++ src/main/java/com/example/SpringBootTestApp/runner/MessageRunner.java
@@ -0,0 +1,14 @@
+package com.example.SpringBootTestApp.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hi.......Hello");
+
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootTestApp/SpringBootTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootTestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
